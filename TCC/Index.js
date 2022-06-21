29cbf691b5d4f90c29ca064ce9527e4bd3611a84
Lucas Machado <101845071+Luksiss@users.noreply.github.com>
2022-06-21 13:18:31 -0300

    Add files via upload

---
+++ TCC/Index.js
@@ -0,0 +1,26 @@
+
+const btnMobile = document.getElementById('btn-mobile')
+
+function toggleMenu(){
+
+    
+    const nav = document.getElementById('menu')
+
+nav.classList.add('active')
+
+
+}
+
+
+
+addEventListener.btnMobile('click', toggleMenu)
+
+
+
+
+
+
+
+
+
+
